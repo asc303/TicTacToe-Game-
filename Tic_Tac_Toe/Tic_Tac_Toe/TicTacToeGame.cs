86680e965a550b67568b4e2b321ada08f80cf715
asc303 <chauhansam94@gmail.com>
2021-12-04 10:29:08 +0530

    [ashutosh] add:Player Game

--- Tic_Tac_Toe/Tic_Tac_Toe/TicTacToeGame.cs
+++ Tic_Tac_Toe/Tic_Tac_Toe/TicTacToeGame.cs
@@ -14,5 +14,23 @@ namespace Tic_Tac_Toe
                 GAMEdemo[i] = ' ';
             }   
         }
+        public void PlayerInput()
+        {
+            Console.WriteLine("Enter Your  Play the Games");
+            char playerInput = Convert.ToChar(Console.ReadLine());
+            char input = char.ToUpper(playerInput);
+            if (input == 'X')
+            {
+                Console.WriteLine("Player Choose :" + playerInput);
+                Console.WriteLine("System Choose :" + 'O');
+            }
+            if (input == 'O')
+            {
+                Console.WriteLine("Player Choose :" + playerInput);
+                Console.WriteLine("System Choose :" + 'X');
+            }
+            else
+                Console.WriteLine("Plese Provide Correct Input to Player Games");
+        }
     }
 }
