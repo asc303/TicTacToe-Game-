dd8e14dcb2f54db1acf2cdc83b82560f5bc3f349
asc303 <chauhansam94@gmail.com>
2021-12-04 09:42:36 +0530

    [ashutosh] add:Tic Tac Toe

---
+++ Tic_Tac_Toe/Tic_Tac_Toe/TicTacToeGame.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tic_Tac_Toe
+{
+   public class TicTacToeGame
+    {
+        public char[] GAMEdemo = new char[10];
+        public void CreateGamedemo() 
+        {
+            for (int i = 1; i < GAMEdemo.Length; i++)
+            {
+                GAMEdemo[i] = ' ';
+            }   
+        }
+    }
+}
